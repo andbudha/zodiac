7774cc0a0011fef3ac601fa9af69cd4f32a1509d
andbudha <andreibartov@gmail.com>
2024-09-16 09:04:42 +0200

    app created and initialized

---
+++ README.md
@@ -0,0 +1,6 @@
+# This Is a Test Git Collaboration Project on Zodiac & Zodiac Signs
+
+- **react.js**
+- **typescript**
+- **sass-preprocessor**
+- **react router**
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import styles from './App.module.scss';
+
+export const App = () => {
+  return (
+    <div className={styles.main_app_box}>
+      <h1>Zodiac Project</h1>
+    </div>
+  );
+};

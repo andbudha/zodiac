4dd87c543e9695a1f29a15fbe21eff193f84d0ae
andbudha <andreibartov@gmail.com>
2024-09-19 16:26:04 +0200

    data fetched and set

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,8 +2,15 @@ import { Outlet } from 'react-router-dom';
 import { Navbar } from '../Navbar/Navbar';
 import styles from './Layout.module.scss';
 import { Footer } from '../Footer/Footer';
+import { useContext, useEffect } from 'react';
+import { DataContext } from '../../context/DataContext';
 
 export const Layout = () => {
+  const { getData } = useContext(DataContext);
+
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <div className={styles.main_layout_box}>
       <Navbar />

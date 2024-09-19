c13e324dbd147d3693d57345dd5ca44ca15b9dc5
Baseer Rahimi <mohammadbaseer25@gmail.com>
2024-09-19 12:59:49 +0200

    Added Details Page and SCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import styles from './App.module.scss';
+import styles from "./App.module.scss";
+import Details from "./components/Details/Details";
 
 export const App = () => {
   return (
     <div className={styles.main_app_box}>
       <h1>Zodiac Project</h1>
+      <Details />
     </div>
   );
 };

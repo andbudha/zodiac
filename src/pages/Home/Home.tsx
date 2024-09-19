228df7a6344f9b5bbca7bc6d39a49021f61a89f1
andbudha <andreibartov@gmail.com>
2024-09-19 14:27:15 +0200

    Home page component created&styled

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,10 @@
+import styles from './Home.module.scss';
+
+export const Home = () => {
+  return (
+    <div className={styles.main_home_box}>
+      <h1 className={styles.header}>Zodiac Signs</h1>
+      <div className={styles.main_slide_box}>slider under construction....</div>
+    </div>
+  );
+};

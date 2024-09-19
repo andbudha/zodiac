1e2d5b32bb55d043c70f9c8c0662c370b1978b2a
Baseer Rahimi <mohammadbaseer25@gmail.com>
2024-09-19 13:50:56 +0200

    App.tsx Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
 import styles from "./App.module.scss";
 import { Layout } from "./components/Layout/Layout";
+import Signs from "./components/Signs/Signs";
+import Details from "./components/Details/Details";
 
 export const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Layout />}>
-        <Route path="signs" element={"signs component"} />
+        <Route path="signs" element={<Signs />} />
+        <Route path="details" element={<Details />} />
       </Route>
     )
   );

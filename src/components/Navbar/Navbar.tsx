13b28823593f6679ec9b65e3d396c687488021e4
andbudha <andreibartov@gmail.com>
2024-09-23 10:03:25 +0200

    displaying active links in navbar func-ty added

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,20 +1,35 @@
 import styles from './Navbar.module.scss';
 import logo from '../../assets/images/logo.png';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
 export const Navbar = () => {
+  const { pathname } = useLocation();
+  console.log(pathname);
+
   return (
     <div className={styles.main_navbar_box}>
       <NavLink to={'/'} className={styles.logo_box}>
         <img src={logo} alt="logo_image" className={styles.logo} />
       </NavLink>
       <div className={styles.links_box}>
-        <NavLink to={'/'} className={styles.link}>
-          home
-        </NavLink>
-        <NavLink to={'/signs'} className={styles.link}>
-          signs
-        </NavLink>
+        <div
+          className={`${styles.link_box} ${
+            pathname === '/' && styles.active_link_box
+          }`}
+        >
+          <NavLink to={'/'} className={styles.link}>
+            home
+          </NavLink>
+        </div>
+        <div
+          className={`${styles.link_box} ${
+            pathname === '/signs' && styles.active_link_box
+          }`}
+        >
+          <NavLink to={'/signs'} className={styles.link}>
+            signs
+          </NavLink>
+        </div>
       </div>
     </div>
   );
